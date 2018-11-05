5ac978bcd02ba02c8b74c3051b8a41d39e739599
Jooseung <jsedwardyou@gmail.com>
2018-11-05 22:05:07 +0900

    Map UI: hovering island added

---
+++ Assets/Jooseung/Script/hoverState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hoverState : MonoBehaviour {
+
+    private mapController mapController;
+    private Vector2 originalPosition;
+
+    private void Start()
+    {
+        mapController = this.GetComponentInParent<mapController>();
+        originalPosition = this.transform.position;
+    }
+
+    //TODO: Fix the teleporting map bug
+
+    //When mouse Enters, set the area in the mapcontroller to this gameobject
+    public void onPointerEnter()
+    {
+        mapController.area = this.gameObject;
+    }
+
+    //When mouse pointer exits, set the area to null and the position back to its original position
+    public void onPointerExit()
+    {
+        mapController.area = null;
+        this.transform.position = originalPosition;
+    }
+}
---
+++ Assets/Jooseung/Script/mapController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+
+public class mapController : MonoBehaviour {
+
+    public GameObject area;
+    public float oscillationHeight;
+
+    private float startTime = 0;
+
+    private void Update()
+    {
+        if (area == null)
+        {
+            startTime = Time.fixedTime;
+            return;
+        }
+            
+
+        pointerOnEffect(area, startTime);
+        
+    }
+
+    private void pointerOnEffect(GameObject area, float startTime)
+    {
+        float y = oscillationHeight * Mathf.Sin((Time.fixedTime - startTime) * 5);
+        area.transform.position = new Vector2(area.transform.position.x, area.transform.position.y + y);
+    }
+}
