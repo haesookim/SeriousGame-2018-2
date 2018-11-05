f682b9a3325424b27734c2b42289a2fb0c59c7c9
Jooseung <jsedwardyou@gmail.com>
2018-11-05 23:51:27 +0900

    NPC with question mark added to the demo scene

---
+++ Assets/Jooseung/Script/General/Util.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Util{
+
+    public static void icon_float(GameObject obj, float oscillationHeight)
+    {
+        float y = oscillationHeight * Mathf.Sin((Time.fixedTime) * 5);
+        obj.transform.position = new Vector2(obj.transform.position.x, obj.transform.position.y + y);
+    }
+	
+}
---
+++ Assets/Jooseung/Script/NPC/npcController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npcController : MonoBehaviour {
+
+    private bool hasQuest = true;
+    public float oscillationHeight;
+
+    public GameObject questMark;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (hasQuest)
+        {
+            questMark.SetActive(true);
+            questMark.transform.position = this.transform.position + new Vector3(0, 1f,0);
+            Util.icon_float(questMark, oscillationHeight);
+        }
+        else
+        {
+            questMark.SetActive(false);
+        }
+	}
+}
