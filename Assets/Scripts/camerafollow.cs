a9cde09d85f7af189855d2fcb1c02f04b8d45967
Heather Kim <haesoo1108@gmail.com>
2018-11-07 00:39:51 +0900

    Pull and initial changes

---
+++ Assets/Scripts/camerafollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour {
+
+    public Transform player;
+    public bool init = true;
+    public Vector3 originpos;
+
+    void Start()
+    {
+        originpos = transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate () {
+        if (init){
+            if(player.position.x > originpos.x){
+                Vector3 newpos = new Vector3(player.position.x, transform.position.y, transform.position.y);
+                transform.position = newpos;
+                init= !init;
+            }
+        }
+	}
+}
