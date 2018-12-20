361842842cdbf4f3ef5e7895ce3c38d7cc6b8f71
Jooseung <jsedwardyou@gmail.com>
2018-12-20 17:20:27 +0900

    project cleaned

---
+++ Assets/Outdated/_Scripts/houseScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class houseScript : MonoBehaviour {
+
+    public float health;
+    public Sprite destroyed;
+
+	// Use this for initialization
+    void Start () {
+        health = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (health < 0){
+            gameObject.GetComponent<SpriteRenderer>().sprite = destroyed;
+        }
+	}
+}
